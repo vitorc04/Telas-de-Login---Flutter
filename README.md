618b51b87ed5cb6b201a2710accd2d176bb7bc47
vitor coimbra <vitorc04@gmail.com>
2021-04-29 16:39:54 -0300

    Telas

---
+++ README.md
@@ -0,0 +1,7 @@
+# FLUTTER APP
+
+1. Tela Principal
+2. Tela de Cadastro
+3. Tela de Login
+
+-- Telas desenvolvidas para a disciplina de implementação de sistemas móveis.
---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:login_signup_screen/login.dart';
+import 'package:login_signup_screen/signup.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          // media query heigth > tamanho
+          // double.infinity deixar maior que o parametro pai permitir
+          // while MediaQuery maior que a tela permitir
+
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            // even space distribution
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Bem vindo",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+
+                    ),
+                    
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Telas desenvolvidas para a disciplina de Implementação de Sistemas Móveis! ",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 15,
+
+                  ),)
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/welcome.png")
+                  )
+                ),
+              ),
+
+              Column(
+                children: <Widget>[
+                  // the login button
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18
+                      ),
+                    ),
+                  ),
+                  // creating the signup button
+                  SizedBox(height:20),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));
+
+                    },
+                    color: Color(0xff0095FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Cadastrar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18
+                      ),
+                    ),
+                  )
+
+                ],
+              )
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
